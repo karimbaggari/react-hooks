d4bfaf507658718acc7f5164704533237f14088b
BAGGARI <karim.baggari@capgemini.com>
2023-06-22 03:04:09 +0100

    initial commit for useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState, useEffect } from "react";
 function App() {
+  const [resourceType, setResourceType] = useState("");
+  const [items, setItems] = useState([]);
+  const [windowsWith, setWindowsWith] = useState(window.innerWidth);
+
+  const handleSize = () => {
+    setWindowsWith(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleSize);
+
+    return () => {
+      window.removeEventListener("resize", handleSize);
+    };
+  }, []);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => {
+        const dataArray = Object.values(json);
+        console.log(resourceType);
+        setItems(dataArray);
+      });
+    return () => {
+      console.log("return from resource changed");
+    };
+  }, [resourceType]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>{windowsWith}</div>
+      <button onClick={() => setResourceType(1)}>first Article</button>
+      <button onClick={() => setResourceType(2)}>second Article</button>
+      <button onClick={() => setResourceType(3)}>third Article</button>
+      <button onClick={() => setResourceType("")}>reset</button>
+      {items.map((item, key) => (
+        <pre key={key}>{JSON.stringify(item)}</pre>
+      ))}
+    </>
   );
 }
 
