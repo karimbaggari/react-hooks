e1d54f58ed0532d9644dd708fb8a3dba68840c22
BAGGARI <karim.baggari@capgemini.com>
2023-06-26 16:57:19 +0100

    initial commit for useTransition

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useTransition } from "react";
 
 function App() {
+  const [isPending, startTransition] = useTransition();
+  const [input, setInput] = useState("");
+  const [list, setList] = useState([]);
+
+  const LIST_SIZE = 20000;
+
+  function handleChange(e) {
+    setInput(e.target.value);
+    startTransition(() => {
+      const l = [];
+      for (let i = 0; i < LIST_SIZE; i++) {
+        l.push(e.target.value);
+      }
+      setList(l);
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type="text" value={input} onChange={handleChange} />
+      {isPending
+        ? "loading"
+        : list.map((item, index) => {
+            return <div key={index}>{item}</div>;
+          })}
+      ;
+    </>
   );
 }
 
