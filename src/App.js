17d9f92d968de7130a250d0697ed21d87059e272
BAGGARI <karim.baggari@capgemini.com>
2023-06-26 15:22:52 +0100

    initial commit CustomHook

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
-import React, { useState, useCallback } from "react";
-import List from "./List";
-
-const ACTIONS = {
-  INCREMENT: "Increment",
-  DECREMENT: "Decrement",
-};
+import React, { useState } from "react";
+import useLocalStorage from "./useLocalStorage";
 
 export default function App() {
-  const [number, setNumber] = useState(1);
-  const [dark, setDark] = useState(false);
-
-  const getItems =useCallback(() => {
-    return [number, number + 1, number + 2];
-  },[number]);
-
-  const theme = {
-    backgroundColor: dark ? "#333" : "#FFF",
-    color: dark ? "#FFF" : "#333",
-  };
+  const [name, setName] = useLocalStorage("name","");
 
   return (
-    <div style={theme}>
-      <input
-        type="number"
-        value={number}
-        onChange={(e) => setNumber(parseInt(e.target.value))}
-      />
-      <button onClick={() => setDark((prevDark) => !prevDark)}>
-        Toggle Theme
-      </button>
-      <List getItems={getItems} />
-    </div>
+    <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
   );
 }
