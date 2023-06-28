875c0696a9dc88d523a4bee732bcfc3949e912de
BAGGARI <karim.baggari@capgemini.com>
2023-06-28 15:52:20 +0100

    initial commit for useId

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
 import { useState, useTransition } from "react";
-
+import EmailForm from "./EmailForm";
 function App() {
-  const [isPending, startTransition] = useTransition();
-  const [input, setInput] = useState("");
-  const [list, setList] = useState([]);
-
-  const LIST_SIZE = 20000;
-
-  function handleChange(e) {
-    setInput(e.target.value);
-    startTransition(() => {
-      const l = [];
-      for (let i = 0; i < LIST_SIZE; i++) {
-        l.push(e.target.value);
-      }
-      setList(l);
-    });
-  }
-
   return (
     <>
-      <input type="text" value={input} onChange={handleChange} />
-      {isPending
-        ? "loading"
-        : list.map((item, index) => {
-            return <div key={index}>{item}</div>;
-          })}
-      ;
+      <EmailForm />
+      <article style={{ marginBlock: "1rem" }}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum
+      </article>
+      <EmailForm />
     </>
   );
 }
